3ae632f742bce37801e53a41f1cb6da7c35885c8
NateBigStone <n.gaut@bigstonemgmt.com>
2020-11-21 23:00:31 -0600

    video list

--- video_collection/urls.py
+++ video_collection/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add', views.add, name='add_video'),
+    path('video_list', views.video_list, name='video_list'),
 ]
